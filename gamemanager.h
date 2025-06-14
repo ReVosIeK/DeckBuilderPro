ed674d9a56b1baa371423161c181a507b536c129
ReVosIeK <mrespa27@gmail.com>
2025-06-14 23:05:49 +0200

    Implementacja przygotowania talii w GameManager

--- gamemanager.h
+++ gamemanager.h
@@ -3,6 +3,8 @@
 
 #include <QObject>
 #include <QList>
+#include <QMap>
+#include <QString>
 #include "CardLoader.h"
 #include "Player.h"
 
@@ -12,29 +14,23 @@ class GameManager : public QObject
 public:
     explicit GameManager(QObject *parent = nullptr);
 
-    // Główna publiczna metoda do inicjalizacji nowej gry
     void setupNewGame(int playerCount);
 
 private:
-    // Prywatne metody pomocnicze, które będą wywoływane przez setupNewGame
     void loadAndPrepareCards();
+    void prepareDecks(); // <-- Nowa metoda do sortowania kart
     void createPlayers(int count);
     void dealStartingHands();
 
-    // Komponenty gry
     CardLoader m_cardLoader;
 
-    // Przechowujemy tutaj wskaźniki na wszystkie unikalne karty w grze
-    QList<Card*> m_allCards;
-    QMap<QString, Card*> m_cardsById; // Dla szybkiego dostępu po ID
+    QMap<QString, Card*> m_cardsById;
 
-    // Stosy i talie gry
     QList<Card*> m_mainDeck;
     QList<Card*> m_kickStack;
     QList<Card*> m_weaknessStack;
     QList<Card*> m_superVillainStack;
 
-    // Gracze
     QList<Player*> m_players;
     Player* m_currentPlayer;
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <QCoreApplication>
 #include <QDebug>
-#include "GameManager.h" // <<< ZMIANA
+#include "GameManager.h"
+#include "Card.h" // Potrzebne do qRegisterMetaType
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
+    // Rejestracja typu, aby qDebug potrafił go obsłużyć
+    qRegisterMetaType<Card::CardType>("Card::CardType");
+
+
     qDebug() << "====== URUCHAMIANIE APLIKACJI ======";
 
     GameManager game;
-    game.setupNewGame(2); // Uruchamiamy setup dla 2 graczy
+    game.setupNewGame(2);
 
     qDebug() << "\n====== APLIKACJA ZAKOŃCZYŁA DZIAŁANIE ======";
 
